9aa84ff14cbcda7ba46386e961949cbd9d323a51
NYehor <yehorhedbalo@gmail.com>
2020-08-31 00:02:59 +0300

    Addregistration form.

--- PetProject/client/src/app/app.module.ts
+++ PetProject/client/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { ThingCardComponent } from './thing-card/thing-card.component';
-import { ThingPageComponent } from './thing-page/thing-page.component';
-import { ThingListComponent } from './thing-list/thing-list.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { ThingCardComponent } from './components/thing-card/thing-card.component';
+import { ThingListComponent } from './components/thing-list/thing-list.component';
+import { UserTabsComponent } from './components/profile-page/user-tabs/user-tabs.component';
+import { UserInfoComponent } from './components/profile-page/user-info/user-info.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     ThingCardComponent,
-    ThingPageComponent,
-    ThingListComponent
+    ThingListComponent,
+    UserTabsComponent,
+    UserInfoComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
--- PetProject/client/src/app/components/profile-page/user-info/user-info.component.spec.ts
+++ PetProject/client/src/app/components/profile-page/user-info/user-info.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ThingPageComponent } from './thing-page.component';
+import { UserInfoComponent } from './user-info.component';
 
-describe('ThingPageComponent', () => {
-  let component: ThingPageComponent;
-  let fixture: ComponentFixture<ThingPageComponent>;
+describe('UserInfoComponent', () => {
+  let component: UserInfoComponent;
+  let fixture: ComponentFixture<UserInfoComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ThingPageComponent ]
+      declarations: [ UserInfoComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ThingPageComponent);
+    fixture = TestBed.createComponent(UserInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
